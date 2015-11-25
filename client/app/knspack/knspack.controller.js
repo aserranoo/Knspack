b0e0d912dcdb8cb191d149ceed08f2d96b034a4e
aserranoo <xas_serranox@hotmail.com>
2015-11-25 05:30:23 -0600

    Update:

--- client/app/knspack/knspack.controller.js
+++ client/app/knspack/knspack.controller.js
@@ -5,93 +5,149 @@
 
     function KnspackController($scope, $http, socket) {
         var ws = this;
-        ws.obj = {};
-        ws.solutions = {Caja1: "",Caja2: "",Caja3: "",Caja4: "",Caja5: "",Caja6: "",Caja7: "",Caja8: ""};
-        var items = [],
-            max_val = 0, 
-            solutions = [],          
-            i, item, val;
-        ws.solutions = [];
-        ws.obj.Volumen = 5000
+        ws.volumenChecked = [{
+            volumen: 11000,
+            check: false
+        }, {
+            volumen: 6000,
+            check: false
+        }, {
+            volumen: 8000,
+            check: false
+        }, {
+            volumen: 9000,
+            check: false
+        }, {
+            volumen: 10000,
+            check: false
+        }];
         ws.objetos = [{
             Descripcion: 'Caja1',
-            Area: 3192,
-            Precio: 7
+            Altura: "",
+            Largo: "",
+            Volumen: 3192,
+            Beneficio: 7,
+            Ruta: "",
+            MenorA: 5,
+            MayorA: 1,
+            Checked: true
         }, {
             Descripcion: 'Caja2',
-            Area: 231,
-            Precio: 11
+            Altura: "",
+            Largo: "",
+            Volumen: 231,
+            Beneficio: 11,
+            Ruta: "",
+            MenorA: 4,
+            MayorA: 1,
+            Checked: true
         }, {
             Descripcion: 'Caja3',
-            Area: 930,
-            Precio: 8
+            Altura: "",
+            Largo: "",
+            Volumen: 930,
+            Beneficio: 8,
+            Ruta: "",
+            MenorA: 4,
+            MayorA: 1,
+            Checked: true
         }, {
             Descripcion: 'Caja4',
-            Area: 350,
-            Precio: 2
+            Altura: "",
+            Largo: "",
+            Volumen: 350,
+            Beneficio: 2,
+            Ruta: "",
+            MenorA: 5,
+            MayorA: 1,
+            Checked: true
         }, {
             Descripcion: 'Caja5',
-            Area: 960,
-            Precio: 12
+            Altura: "",
+            Largo: "",
+            Volumen: 960,
+            Beneficio: 12,
+            Ruta: "",
+            MenorA: 7,
+            MayorA: 1,
+            Checked: true
         }, {
             Descripcion: 'Caja6',
-            Area: 2880,
-            Precio: 30
+            Altura: "",
+            Largo: "",
+            Volumen: 2880,
+            Beneficio: 30,
+            Ruta: "",
+            MenorA: 8,
+            MayorA: 1,
+            Checked: true
         }];
-        ws.agregarElementos = agregarElementos;
-        ws.eliminarElementos = eliminarElementos;
-        ws.optimizarEspacio = optimizarEspacio;
 
-        function agregarElementos(data, $index) {
-            var datos = {};
-            datos = {
-                Descripcion: data.Descripcion,
-                Area: data.Area,
-                Precio: data.Precio
-            }
-            ws.objetos.push(datos);
+        ws.solutions = [];
+        ws.volumenRestante;
+        ws.beneficioTotal = 0;
+        ws.optimizarEspacio = optimizarEspacio;
+        ws.checkVol = checkVol;
+        ws.obtenerVol = obtenerVol;
+        ws.obtenerSolucionInicial = obtenerSolucionInicial;    
+
+        function checkVol(index) {
+            _.forEach(ws.volumenChecked, function(value, key) {
+                if (key == index) {
+                    ws.volumenChecked[key].check = true;
+                } else {
+                    ws.volumenChecked[key].check = false;
+                }
+            })
         }
 
-        function eliminarElementos($index) {
-            ws.objetos.splice($index, 1);
+        function obtenerVol() {
+            var vol = 0;
+            _.forEach(ws.volumenChecked, function(value, key) {
+                if (value.check) {
+                    ws.volumenRestante = value.volumen
+                }
+            });
+        }
+
+        function obtenerSolucionInicial(objetos) {
+            _.forEach(objetos, function(value, key) {
+                if (value.Checked) {
+                    ws.solutions.push({
+                        Descripcion: value.Descripcion,
+                        Cantidad: value.MayorA,
+                        CantidadMax: value.MenorA,
+                        Volumen: value.Volumen,
+                        Beneficio: value.Beneficio
+                    })
+                }
+            });            
+            _.forEach(ws.solutions, function(value, key){
+                ws.beneficioTotal = ws.beneficioTotal + (value.Cantidad * value.Beneficio);
+                ws.volumenRestante = ws.volumenRestante - (value.Cantidad * value.Volumen);                
+            });            
+            return ws.solutions
         }
 
         function optimizarEspacio() {
-            for (i = 0; i < ws.objetos.length; i += 1) {
-                item = ws.objetos[i];
-                item.max = Math.min(
-                    Math.floor(ws.obj.Volumen / ws.objetos[i].Area)
-                );
-            }
-
-            for (var a = 0; a <= ws.objetos[0].max; a++) {
-                for (var b = 0; b <= ws.objetos[1].max; b++) {
-                    for (var c = 0; c <= ws.objetos[2].max; c++) {
-                        for (var d = 0; d < ws.objetos[3].max; d++) {
-                            for (var e = 0; e < ws.objetos[4].max; e++) {
-                                for (var g = 0; g < ws.objetos[5].max; g++) {                                 
-                                    if (g * ws.objetos[5].Area + a * ws.objetos[0].Area + b * ws.objetos[1].Area + c * ws.objetos[2].Area + d * ws.objetos[3].Area + e * ws.objetos[4].Area > ws.obj.Volumen) {
-                                        continue;
-                                    }
-                                    val = g * ws.objetos[5].Precio + a * ws.objetos[0].Precio + b * ws.objetos[1].Precio + c * ws.objetos[2].Precio + d * ws.objetos[3].Precio + e * ws.objetos[4].Precio;
-                                    if (val > max_val) {
-                                        solutions = [];
-                                        max_val = val;
-                                    }
-                                    if (val === max_val) {
-                                        ws.solutions.push({Caja1: g, Caja2: a,Caja3: b, Caja4: c, Caja5: d, Caja6: e, Caja7: g});
-                                    }
-                                };
-                            };
-                        };
-                    };
-                };
-            };
-            console.log("maximum value: " + max_val + '<br>');
-            for (i = 0; i < solutions.length; i += 1) {
-                item = solutions[i];                
-                document.write("(gold: " + item[0] + ", panacea: " + item[1] + ", ichor: " + item[2] + ")<br>");
-            };
-        };
+            var solucionBeta = {}
+            var vol = 0;
+            var tmpVol = 0;
+            obtenerVol();
+            ws.solutions = [];
+            var iteraciones = [];
+            solucionBeta = obtenerSolucionInicial(ws.objetos);        
+            _.forEach(solucionBeta, function(value, key) {
+                while (value.Cantidad * value.Volumen <= ws.volumenRestante && value.Cantidad <= value.CantidadMax) {                    
+                    solucionBeta[key].Cantidad += 1;
+                    vol = vol + solucionBeta[key].Volumen;
+                    ws.beneficioTotal = ws.beneficioTotal + solucionBeta[key].Beneficio;
+                    ws.volumenRestante = ws.volumenRestante - solucionBeta[key].Volumen;                    
+                    iteraciones.push(value)
+                }
+            });    
+            console.log(iteraciones)        
+            console.log(ws.solutions, vol, ws.volumenRestante, ws.beneficioTotal);
+        }
     }
 })();
